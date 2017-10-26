cf500113ed296d1103c5c44c8c6ad935734d53f4
teovoinea <voinea.teodor@gmail.com>
2017-10-25 23:43:58 -0400

    Get some metrics on fps

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 4242
@@ -6,7 +7,10 @@ UDP_PORT = 4242
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+start_time = time.time()
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print(data)
+    data, addr = sock.recvfrom(1500) # buffer size is 1024 bytes
+    
+    print (time.time() - start_time)
+    start_time = time.time()
